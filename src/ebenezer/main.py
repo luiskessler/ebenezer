d6edcebaf2d4aa3e76a2790f2c7060ef43a79fc8
luiskessler <105922569+luiskessler@users.noreply.github.com>
2026-02-21 23:51:52 +0100

    setup

---
+++ src/ebenezer/main.py
@@ -0,0 +1,16 @@
+class EbenezerClassifier:
+    def __init__(self):
+        pass
+    
+    def load():
+        """
+        Load the model
+        Exposes model.classify
+        """
+        pass
+    
+    def classify():
+        """
+        
+        """
+        pass
